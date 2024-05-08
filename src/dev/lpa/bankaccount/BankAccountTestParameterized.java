e9f3f6ddeb274dc1bce6dd66bfbd470b4958adfc
gil.00000032610@unicap.br <gil.00000032610@unicap.br>
2024-05-08 14:59:56 -0300

    no custom constructor allowed and Stream<Argument>

--- src/dev/lpa/bankaccount/BankAccountTestParameterized.java
+++ src/dev/lpa/bankaccount/BankAccountTestParameterized.java
@@ -1,59 +1,43 @@
 package dev.lpa.bankaccount;
 
-import org.junit.Before;
-import org.junit.Test;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
-import org.junit.runner.RunWith;
-import org.junit.runners.Parameterized;
 
-import java.util.Arrays;
-import java.util.Collection;
 import java.util.stream.Stream;
 
-import static org.junit.Assert.assertEquals;
 
-@RunWith(Parameterized.class)
 public class BankAccountTestParameterized {
 
     private BankAccount account;
-    private double amount;
-    private boolean branch;
-    private double expected;
 
 
-    public BankAccountTestParameterized(double amount, boolean branch, double expected) {
-        this.amount = amount;
-        this.branch = branch;
-        this.expected = expected;
-    }
-
-    @Before
+    @BeforeEach
     public void setUp() {
         account = new BankAccount("Gil", "Araújo", 6700.0, BankAccount.CHECKING);
         System.out.println("Running test");
     }
 
-    @Parameterized.Parameters
-    public static Collection<Object[]> testConditions() {
-        return Arrays.asList(new Object[][]{
-                {100.00, true, 6800.00},
-                {200.00, true, 6900.00},
-                {113.90, true, 6813.90},
-                {99.99, true, 6799.99}
-        });
-    }
+//    @Parameterized.Parameters
+//    public static Collection<Object[]> testConditions() {
+//        return Arrays.asList(new Object[][]{
+//                {100.00, true, 6800.00},
+//                {200.00, true, 6900.00},
+//                {113.90, true, 6813.90},
+//                {99.99, true, 6799.99}
+//        });
+//    }
 
-    @Test
-    public void deposit() {
-        double balance = account.deposit(amount, branch);
-        assertEquals(expected, account.getBalance(), 0.01);
-    }
+//    @Test
+//    public void deposit() {
+//        double balance = account.deposit(amount, branch);
+//        assertEquals(expected, account.getBalance(), 0.01);
+//    }
 
 
-    // in JUnit 5 we use Streams to parameterized Tests along with the MethodSource
+    // in JUnit 5 we use Stream<Argument> to parameterized Tests along with the MethodSource
 
 
     @ParameterizedTest
